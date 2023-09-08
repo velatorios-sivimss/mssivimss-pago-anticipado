a65be58cfbfafb6ec223adc6152af8434a9033db
Eduardo Orlando <eduardo.flores@people-media.com.mx>
2023-09-07 19:03:56 -0600

    se corrige restante que muestra al modificar pago

--- src/main/java/com/imss/sivimss/pagoanticipado/beans/BusquedasPlanSFPA.java
+++ src/main/java/com/imss/sivimss/pagoanticipado/beans/BusquedasPlanSFPA.java
@@ -175,8 +175,8 @@ public class BusquedasPlanSFPA {
                         "PAQ.DES_NOM_PAQUETE AS nombrePaquete","CONCAT(SP.NOM_PERSONA , ' ', SP.NOM_PRIMER_APELLIDO, ' ', SP.NOM_SEGUNDO_APELLIDO) AS contratanteSubstituto"
                 ,"SP.DES_CORREO AS correo","SD.DES_ESTADO  AS estado","VEL.DES_VELATORIO AS velatorio","EST.DES_ESTATUS_PLAN_SFPA AS estatusPlan",
                 "PAQ.MON_PRECIO AS total",
-                        "(SELECT SBPA.DES_TOTAL_RESTANTE AS restante FROM SVC_BITACORA_PAGO_ANTICIPADO SBPA WHERE SBPA.ID_BITACORA_PAGO =(SELECT MAX(PA.ID_BITACORA_PAGO) FROM" +
-                                " SVC_BITACORA_PAGO_ANTICIPADO PA WHERE PA.ID_PLAN_SFPA = " + idPlan + " AND PA.IND_ACTIVO =1)) AS restante")
+                        "(SELECT (PLAN.MON_PRECIO - SUM(SBPA.DES_IMPORTE)) AS restante FROM SVC_BITACORA_PAGO_ANTICIPADO SBPA LEFT JOIN SVT_PLAN_SFPA PLAN on SBPA .ID_PLAN_SFPA" +
+                                " = PLAN.ID_PLAN_SFPA WHERE SBPA.ID_PLAN_SFPA = " + idPlan + " ) AS restante")
                 .from("SVT_PLAN_SFPA SPS")
                 .leftJoin("SVC_TIPO_PAGO_MENSUAL TPM","SPS.ID_TIPO_PAGO_MENSUAL = TPM.ID_TIPO_PAGO_MENSUAL")
                 .leftJoin("SVT_PAQUETE PAQ","SPS.ID_PAQUETE = PAQ.ID_PAQUETE")
